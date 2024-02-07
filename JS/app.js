ba0e0813b8a99b85df1d9bdce4c17c94faa70e75
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-02-07 03:03:33 +0300

    addEnterEvent

--- JS/app.js
+++ JS/app.js
@@ -36,18 +36,19 @@ console.log("app.js is ACTIVE FILE")
         }
         // PHONE DISPLAY 
         phones.forEach(phone => {
-            console.log(phone)
+            // console.log(phone)
             const phoneDiv = document.createElement("div")
             phoneDiv.classList.add("col")
             phoneDiv.innerHTML = `
             <div class="card rounded">
-            <img src="${phone.image}" class="card-img-top p-3" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">${phone.phone_name}</h5>
-            <p class="card-text">
-            This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-            </p>
-            </div>
+                <img src="${phone.image}" class="card-img-top p-3" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">
+                        This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                    </p>
+                    <button onclick="loadPhoneDetailes('${phone.slug}')" type="button" class="btn btn-info w-100">Info</button>
+                </div>
             </div>
             `;
             phoneContainer.appendChild(phoneDiv);
@@ -68,8 +69,14 @@ console.log("app.js is ACTIVE FILE")
     document.getElementById('searchBtn').addEventListener('click', function(){
         allProduct(10)
     })
-    
-    
+
+    // ===========================================  PRESS ENTER OF RESULT 
+    document.getElementById('inputSearch').addEventListener('keydown', function (e) {
+            if (e.key === 'Enter'){
+                allProduct(10)
+            }
+    })
+
     // ===========================================  LOADING UNTILL LOAD FROM API
     const toggleLoader = isLoading => {
         const loader = document.getElementById('loader')
@@ -90,7 +97,13 @@ console.log("app.js is ACTIVE FILE")
 
 
 
+    const loadPhoneDetailes = async id => {
 
+        const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+        const res = await fetch(url)
+        const data = await res.json()
+        console.log(data.data   )
+    }
 
 
 
